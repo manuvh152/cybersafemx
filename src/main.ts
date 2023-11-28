e04ef92e93bac625563d83c76d0f77349373f361
Manuel Alonso <manuvh152@gmail.com>
2023-11-28 00:24:32 -0600

    Port fixed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
  
